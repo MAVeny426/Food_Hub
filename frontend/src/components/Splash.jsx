6f94b8c74822016e14ffdef8eea464c350cbe15b
MAVeny426 <venyma0@gmail.com>
2025-06-03 10:19:36 +0530

    commit

---
+++ frontend/src/components/Splash.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../StyleCss/Splash.css'
+
+const Splash = () => {
+  return (
+    <div className="splashdiv">
+       <h1>Welcome to Recipe Hub</h1>
+    </div>
+
+  )
+}
+
+export default Splash
